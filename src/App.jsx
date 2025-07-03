6a7c62a803b9bf1574f17f2f11abc26bc042802c
imani-prog <timothyimani128@gmail.com>
2025-07-03 12:13:25 +0300

    Added products card

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
+import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Layout from './components/Layout'
-import OrderCard from './components/OrderCard'
-import ProductCard from './components/ProductCard'
-import CustomerDetail from './pages/CustomerDetail'
-import CustomerEdit from './pages/CustomerEdit'
-import CustomerOrders from './pages/CustomerOrders'
-import CustomerPayments from './pages/CustomerPayments'
-import Customers from './pages/Customers'
 import Dashboard from './pages/Dashboard'
-import Employees from './pages/Employees'
-import NotFound from './pages/NotFound'
 import Offices from './pages/Offices'
-import OrderDetail from './pages/OrderDetail'
 import Orders from './pages/Orders'
 import Payments from './pages/Payments'
 import Products from './pages/Products'
+import Employees from './pages/Employees'
+import Customers from './pages/Customers'
+import NotFound from './pages/NotFound'
+import ProductCard from './components/ProductCard'
+import OrderCard from './components/OrderCard'
+import Layout from './components/Layout'
 // import Navbar from './components/Navbar'
 
 
@@ -24,14 +20,9 @@ const App = () => {
       <Route element={<Layout />}>
         <Route path="/" element={<Dashboard />} />
         <Route path="/customers" element={<Customers />} />
-        <Route path="/customers/:customerId" element={<CustomerDetail />} />
-        <Route path="/customers/:customerId/edit" element={<CustomerEdit />} />
-        <Route path="/customers/:customerId/orders" element={<CustomerOrders />} />
-        <Route path="/customers/:customerId/payments" element={<CustomerPayments />} />
         <Route path="/employees" element={<Employees />} />
         <Route path="/offices" element={<Offices />} />
         <Route path="/orders" element={<Orders />} />
-        <Route path="/orders/:orderId" element={<OrderDetail />} />
         {/* <Route path="/Navbar" element={<Navbar />} /> */}
         <Route path="/payments" element={<Payments />} />
         <Route path="/products" element={<Products />} />
